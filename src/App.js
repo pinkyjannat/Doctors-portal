5adc2e5ecbeff67cbffe308b3d711fa403f10278
Jannatul Ferdous <jannatpinky92@gmail.com>
2022-05-13 14:54:28 -0400

    first commit with all new components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Login from './Pages/login/Login';
+import NavBar from './Pages/shared/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <NavBar></NavBar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Testimonials from '../Testimonials';
+import Banner from './Banner';
+import Contact from './Contact';
+import Footer from './Footer';
+import Info from './Info';
+import MakeAppointment from './MakeAppointment';
+import Services from './Services';
+
+const Home = () => {
+    return (
+        <div>
+         <Banner></Banner>
+         <Info></Info>
+         <Services></Services>
+         <MakeAppointment></MakeAppointment>
+         <Testimonials></Testimonials>
+         <Contact></Contact>
+         <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
